08078a1b83964705fa8d0eeacbef012e99bea339
Panchita <franciscalandwehr@hotmail.com>
2025-07-08 12:45:42 -0500

    added Shopping  Cart component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Routes, Route} from 'react-router-dom'
+import { useState } from "react";
 import './styles/App.css'
 import './styles/main.css'
 import Header from './components/Header'
@@ -10,14 +11,16 @@ import ChooseYourCake from './pages/ChooseYourCake'
 import FormWrapper from './components/formWrapper'
 import ScrollToTop from './components/ScrollToTop'
 import BuildMyCake from './pages/BuildMyCake'
+import ShoppingCar from './pages/ShoppingCar'
 
 
 function App() {
+  const [isCartOpen, setIsCartOpen] = useState(false)
  return(
   <>
   <ScrollToTop />
   <Header />
-  <SlideInMenu />
+  <SlideInMenu setIsCartOpen={setIsCartOpen} />
   <Routes>
     <Route path="/" element={<LandingPage />} />
     <Route path='/choose' element={<ChooseYourCake />} />
@@ -25,7 +28,7 @@ function App() {
     <Route path='/login' element={<FormWrapper />}/>
     <Route path='/signeup' element={<FormWrapper />}/>
     <Route path='/buildYourOwn' element={<BuildMyCake />}/>
-    
+    <Route path='/shoppingCar' element={<ShoppingCar />}/>
   </Routes>
   <Footer />
 
--- client/src/components/SlideInMenu.jsx
+++ client/src/components/SlideInMenu.jsx
@@ -39,7 +39,7 @@ export default function SlideInMenu() {
               <Link to='/' onClick={()=> setIsOpen(false)}><li><FontAwesomeIcon icon={faHouse}/> Home</li></Link>
               <Link to= '/choose' onClick ={()=> setIsOpen(false)}><li><FontAwesomeIcon icon={faCookie} /> Our Favorite Bakes</li></Link>
               <Link to= '/buildYourOwn' onClick = {()=> setIsOpen(false)}><li><FontAwesomeIcon icon={faCake} /> Build your own Cake</li></Link>
-              <Link to= '' onClick = {()=> setIsOpen(false)}><li><FontAwesomeIcon icon={faShoppingCart} /> Shopping Cart</li></Link>
+              <Link to= '/shoppingCar' onClick = {()=> setIsOpen(false)}><li><FontAwesomeIcon icon={faShoppingCart} /> Shopping Cart</li></Link>
               <Link to= '/contact' onClick = {()=> setIsOpen(false)}><li><FontAwesomeIcon icon={faEnvelope} /> Contact</li></Link>
               <Link to='/login' onClick ={() => setIsOpen(false)}><li><FontAwesomeIcon icon={faRightToBracket} /> Log In</li></Link>
             </ul>
