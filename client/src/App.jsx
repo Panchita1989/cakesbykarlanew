36e6bde2abcc94af617d086cfe5c1117bfcbfc85
Panchita <franciscalandwehr@hotmail.com>
2025-07-14 09:54:50 -0500

    fixing profile.path

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,7 +12,7 @@ import ChooseYourCake from './pages/ChooseYourCake'
 import FormPage from './pages/FormPage'
 import ScrollToTop from './components/ScrollToTop'
 import ShoppingCar from './pages/ShoppingCar'
-import Profile from './pages/Profile'
+import Profile from './pages/Profile.jsx'
 import ThankYouPage from './pages/ThankYouPage'
 
 
