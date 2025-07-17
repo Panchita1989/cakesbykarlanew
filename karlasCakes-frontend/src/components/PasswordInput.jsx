85677c1314627048de039dcbd50e4829f92b974d
Panchita <franciscalandwehr@hotmail.com>
2025-07-17 11:02:57 -0500

    fixed fontAwsome Icon

--- karlasCakes-frontend/src/components/PasswordInput.jsx
+++ karlasCakes-frontend/src/components/PasswordInput.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faEye } from "@fortawesome/free-regular-svg-icons"
+import { faEye } from "@fortawesome/free-solid-svg-icons"
+
 
 export default function PasswordInput({name, required, defaultValue, placeholder}){
     const [visible, setVisible] = useState(false)
