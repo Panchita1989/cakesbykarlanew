f8d691c0063a4460b47800961613c0b4fedf60b4
Panchita <franciscalandwehr@hotmail.com>
2025-07-17 10:53:11 -0500

    fixed bug in formPage

--- karlasCakes-frontend/src/pages/FormPage.jsx
+++ karlasCakes-frontend/src/pages/FormPage.jsx
@@ -7,6 +7,7 @@ export default function FormPage() {
   const [formType, setFormType] = useState(null)
   const [prefillData, setPrefillData] = useState(null)
   const [cakes, setCakes] = useState([])
+  const [cart, setCart] = useState([])
   const API_URL = import.meta.env.VITE_API_URL
 
 
