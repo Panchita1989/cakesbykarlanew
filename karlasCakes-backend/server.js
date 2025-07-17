8055ae7a0e3cfccf5ff52919636be1dfed8a2edc
Panchita <franciscalandwehr@hotmail.com>
2025-07-17 09:49:07 -0500

    added trust proxy

--- karlasCakes-backend/server.js
+++ karlasCakes-backend/server.js
@@ -1,6 +1,7 @@
 
 const express = require('express')
 const app = express()
+app.set('trust proxy', 1)
 const mongoose = require('mongoose')
 const passport = require('passport')
 const session = require('express-session')
